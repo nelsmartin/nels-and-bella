4a6ff68375df9de0c6f996b9ba3e770227b6fb22
Nels Martin <nels@nelsmartin.com>
2025-12-29 19:34:43 -0600

    smooth black and white

--- app/tiles/page.tsx
+++ app/tiles/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-
+// Idea: Start with stripes (continuous blocks), swap = invert as well
 import { useState, useEffect } from "react";
 import Image from "next/image";
 
-const CARD_COUNT = 500
+const CARD_COUNT = 208
 export default function Page() {
     
     const [matrix, setMatrix] = useState<boolean[]>(
@@ -17,16 +17,24 @@ export default function Page() {
             return next;
     });
     }
+    const imageInit = (index : number, value: boolean) => {
+      if (index % 2 == 0) {
+        if (value) return "/tileLeftNormal.svg"
+        return "/tileRightInverted.svg"
+      }
+      if (value) return "/tileLeftInverted.svg"
+      return "/tileRightNormal.svg"
+    } 
     return (
         <main className="min-h-screen w-full flex flex-col items-center bg-blue-200">
-            <div className="w-1/2 grid grid-cols-[repeat(auto-fill,minmax(28px,1fr))] place-items-center">
+            <div className="w-1/2 grid grid-cols-[repeat(auto-fill,minmax(55px,1fr))] place-items-center">
                     {matrix.map((value, index) => (
                       <button
                         key={index}
                         onPointerEnter={() => switchEnter(index)}
                       >
                        <Image
-                       src={value ? "/tileRight.svg" : "/tileLeft.svg"}
+                       src={imageInit(index, value)}
                        alt=""
                        height={100}
                        width={100}
