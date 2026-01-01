ed3d40fa81609d7edafb003ccfa6313f138f83f2
Nels Martin <nels@nelsmartin.com>
2026-01-01 12:42:36 -0600

    added three kinds of tiles

--- app/tiles/page.tsx
+++ app/tiles/page.tsx
@@ -2,50 +2,45 @@
 // Idea: Start with stripes (continuous blocks), swap = invert as well
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link"
+import Bold from "./Bold"
+import Lines from "./lines"
+import Angles from "./Angles"
 
-const CARD_COUNT = 208
 export default function Page() {
     
-    const [matrix, setMatrix] = useState<boolean[]>(
-                () => Array(CARD_COUNT).fill(false));
 
-    const switchEnter = (index : number) => {
-        setMatrix(prev => {
-
-            const next = [...prev];
-            next[index] = !prev[index];
-            return next;
-    });
-    }
-    const imageInit = (index : number, value: boolean) => {
-      if (index % 2 == 0) {
-        if (value) return "/tileLeftNormal.svg"
-        return "/tileRightInverted.svg"
-      }
-      if (value) return "/tileLeftInverted.svg"
-      return "/tileRightNormal.svg"
-    } 
     return (
-        <main className="min-h-screen w-full flex flex-col items-center bg-blue-200">
-            <div className="w-1/2 grid grid-cols-[repeat(auto-fill,minmax(55px,1fr))] place-items-center">
-                    {matrix.map((value, index) => (
-                      <button
-                        key={index}
-                        onPointerEnter={() => switchEnter(index)}
-                      >
-                       <Image
-                       src={imageInit(index, value)}
-                       alt=""
-                       height={100}
-                       width={100}
-                       />
-                            
-                            
-                    
-                      </button>
-                    ))}
-                  </div>
-        </main>)
+        <main className="min-h-screen w-full flex flex-row items-center bg-blue-100">
+          <Link
+          href="/tiles/linesView"
+          >
+          <button 
+            className="group px-10 cursor-pointer"
+          >
+            <Lines/>
+          </button>
+          </Link>
+          <Link
+          href="/tiles/boldView"
+          >
+          <button
+            className="group px-10 cursor-pointer"
+          >
+            <Bold/>
+          </button>
+          </Link>
+          <Link
+          href="/tiles/anglesView"
+          >
+          <button
+            className="group px-10 cursor-pointer"
+          >
+            <Angles/>
+          </button> 
+          </Link>
+        </main>
+      )
 }
     
     
