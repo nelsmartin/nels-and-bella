5777cf3cde62669f55d86be429fe4e6ee076f1a9
Nels Martin <nels@nelsmartin.com>
2026-01-07 17:54:12 -0800

    poem

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col space-y-10 items-center justify-center min-h-screen">
     <Image
-            src="/nelsAndBella.jpeg"
+            src="/bellaHead.jpeg"
             alt="scroll image"
             width={1088}
             height={1088}
             className="w-70 h-auto"
           />
-
+    <p className="text-2xl">
+      Little Bella stuck in class. 
+    </p>
+    <p className="text-2xl">
+      Sorry Bella. That's real Ass.
+    </p>
+    <p className="text-2xl">
+      To be stuck in that tiny class.
+    </p>
+    <p className="text-2xl">
+      Ass ass ass ass ass ass ass.
+    </p>
     </div>
     
   );
